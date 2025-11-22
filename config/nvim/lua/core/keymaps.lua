f73ecedb3db880171bf644d56a20581417a7e348
Leonardo Saito <leosaito632@gmail.com>
2025-11-22 00:27:53 -0300

    diagnostic keybind removed from keymaps

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -27,30 +27,30 @@ vim.keymap.set("n", "<leader>t", ":FloatermNew<CR>", {
 	desc = "Toggle floating terminal (Floaterm)",
 })
 
--- Diagnostic
----@param jumpCount number
-local function jumpWithVirtLineDiags(jumpCount)
-	pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
-
-	vim.diagnostic.jump({ count = jumpCount })
-
-	local initialVirtTextConf = vim.diagnostic.config().virtual_text
-	vim.diagnostic.config({
-		virtual_text = false,
-		virtual_lines = { current_line = true },
-	})
-
-	vim.defer_fn(function() -- deferred to not trigger by jump itself
-		vim.api.nvim_create_autocmd("CursorMoved", {
-			desc = "User(once): Reset diagnostics virtual lines",
-			once = true,
-			group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
-			callback = function()
-				vim.diagnostic.config({ virtual_lines = false, virtual_text = initialVirtTextConf })
-			end,
-		})
-	end, 1)
-end
+-- -- Diagnostic
+-- ---@param jumpCount number
+-- local function jumpWithVirtLineDiags(jumpCount)
+-- 	pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
+--
+-- 	vim.diagnostic.jump({ count = jumpCount })
+--
+-- 	local initialVirtTextConf = vim.diagnostic.config().virtual_text
+-- 	vim.diagnostic.config({
+-- 		virtual_text = false,
+-- 		virtual_lines = { current_line = true },
+-- 	})
+--
+-- 	vim.defer_fn(function() -- deferred to not trigger by jump itself
+-- 		vim.api.nvim_create_autocmd("CursorMoved", {
+-- 			desc = "User(once): Reset diagnostics virtual lines",
+-- 			once = true,
+-- 			group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
+-- 			callback = function()
+-- 				vim.diagnostic.config({ virtual_lines = false, virtual_text = initialVirtTextConf })
+-- 			end,
+-- 		})
+-- 	end, 1)
+-- end
 
 vim.keymap.set("n", "ge", function()
 	jumpWithVirtLineDiags(1)
