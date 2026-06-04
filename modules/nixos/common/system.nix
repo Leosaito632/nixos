ab46565c5f104ba9ccebbb1b423b5a1c383b409d
Leonardo Saito <leosaito632@gmail.com>
2026-06-04 02:54:52 -0300

    noctalia cache

--- modules/nixos/common/system.nix
+++ modules/nixos/common/system.nix
@@ -165,7 +165,12 @@
   ###
   # STATE VERSION
   ###
-
+  nix.settings = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
   system.stateVersion = "25.05";
 
 }
