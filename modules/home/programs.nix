3fffb4178f3cd1fe05111adfadcfdb867a32116d
Leonardo Saito <leosaito632@gmail.com>
2026-08-04 11:13:27 -0300

    remove vscode

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -55,13 +55,13 @@
     };
   };
 
-  programs.vscode = {
-    enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      ms-python.python
-      ms-toolsai.jupyter
-    ];
-  };
+  # programs.vscode = {
+  #   enable = true;
+  #   profiles.default.extensions = with pkgs.vscode-extensions; [
+  #     ms-python.python
+  #     ms-toolsai.jupyter
+  #   ];
+  # };
 
   home.packages = with pkgs; [
     # --- Dev Tools ---
